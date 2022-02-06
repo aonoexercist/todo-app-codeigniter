8455805689f224f7c9c72d313833383be7c19b81
aonoexercist <xercisthegreat@gmail.com>
2022-02-07 02:06:53 +0800

    update data

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
 	<meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>My Todo App</title>
+    <title>Todo App</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('bootstrap/css/bootstrap.css'); ?>"/>
     <style>
